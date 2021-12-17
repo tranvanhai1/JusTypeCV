8ceffbc372cbc1972cc9d795c9a427f37ce2606b
tranvanhai1 <95581748+tranvanhai1@users.noreply.github.com>
2021-12-18 00:03:05 +0700

    Add files via upload

--- web.js
+++ web.js
@@ -1,43 +1,151 @@
-
-$(document).ready(function(){
-    $(".back_right").delay(2000).fadeIn(1000)
+$(window).on('load', function(){
+    var loadbar = document.querySelector(".loader-re");
+    console.log(loadbar)
+    var per = 0;
+    var loading = setInterval(animate, 15);
+    function animate(){
+        if(per == 100){
+            clearInterval(loading);
+            loading = setInterval(animate2, 7);
+            // $("#loader").remove()
+        }else{
+            per = per + 1;
+            loadbar.style.width = per + '%';
+        }    
+    }
+    var per2 = 20;
+    function animate2(){
+        if(per2 == 80){
+            $("#loader").remove()
+            clearInterval(loading);
+        }else{
+            per2 = per2 + 1;
+            loadbar.style.marginLeft = per2 + 'vw';
+        }    
+    }
 })
 
 $(document).ready(function(){
-    $(".button_submit").click(function(){
-        $(".img_web_name").animate({top: '80px'}, 300);
-        $(".img_web_name").animate({top: '-500px'}, 300);
-        $(".img_web_name_CV").animate({top: '150px'}, 300);
-        $(".img_web_name_CV").animate({top: '-500px'}, 300);
-        $(".slogan_web").delay(300).animate({top: '240px'}, 300);
-        $(".slogan_web").animate({top: '-500px'}, 300);
-        $(".input_name").delay(600).animate({top: '300px'}, 300);
-        $(".input_name").animate({top: '-500px'}, 300);
-        $(".input_prof").delay(900).animate({top: '430px'}, 300);
-        $(".input_prof").animate({top: '-500px'}, 300);
-        $(".button_submit").attr("class", "bounce-out-top")
-        $(".img_web_name").delay(1200).slideUp()
-        $(".img_web_name_CV").delay(1200).slideUp()
-        $(".slogan_web").delay(1200).slideUp()
-        $(".input_name").delay(1200).slideUp()
-        $(".input_prof").delay(1200).slideUp()
-        $("#intro_web").delay(1800).fadeOut("slow", function(){
-            $("#CV").addClass("scale-down-center")
-            window.scrollTo({top:235,behavior:'smooth'});
-            $("#CV").delay(2000).animate({left: '250px'}, 500, function(){
-                $("#CV").addClass("scale-up-center")
+    $(".templet").click(function(){
+        if($(".review_templet").hasClass("slide-in-top")){
+            $(".review_templet").removeClass("slide-in-top")
+            $(".review_templet").addClass("slide-out-bottom")
+            $(".templet i").addClass("slide-in-top")
+            $(".templet i").removeClass("slide-out-bottom")
+        }else{
+            $(".review_templet").removeClass("slide-out-bottom")
+            $(".review_templet").addClass("slide-in-top")
+            $(".templet i").addClass("slide-out-bottom")
+            $(".templet i").removeClass("slide-in-top")
+        }
+    })
+})
+
+$(document).ready(function(){
+    $("#CV").addClass("scale-up-center")
+    $(".skip .fas").click(function(){
+        $(this).remove()
+        $(".part1").remove()
+        $(".back_right").delay(100).animate({
+            top : '0'
+        }, 100, function(){
+            $(".back_right").addClass("scale-down-center")
+            $(".back_right").delay(500).animate({width : '52%'}, 500, function(){
+                $(".back_right p").fadeIn(500, function(){
+                    $(".back_right h1").delay(100).fadeIn(200, function(){
+                        $(".name_part3").addClass("text-flicker-in-glow")
+                        $(".about").delay(1300).fadeIn("slow")
+                        $(".templet").delay(1400).fadeIn("slow", function(){
+                            $(".name_part3").css("opacity", "1")
+                            $(".name_part3").removeClass("text-flicker-in-glow")
+                        })
+                    })
+                })
+            })
+        })
+    })
+    $(".part1").delay(2000).animate({
+        width : "100%"
+    }, 100, function(){
+        $(".one").addClass("appear").delay(2100).queue(function(next){
+            $(".one").addClass("fade-out-right")
+            next();
+        });
+    })
+    $(".part1").delay(2300).animate({
+        width : "100%"
+    }, 100, function(){
+        $(".two").addClass("appear").delay(2100).queue(function(next){
+            $(".two").addClass("fade-out-right")
+            next();
+        });
+    })
+    $(".part1").delay(2300).animate({
+        width : "100%"
+    }, 100, function(){
+        $(".three").addClass("appear").delay(2100).queue(function(next){
+            $(".three").addClass("fade-out-right")
+            next();
+            $(".back_right").delay(100).animate({
+                top : '0'
+            }, 100, function(){
+                $(".skip .fas").remove()
+                $(".back_right").addClass("scale-down-center")
+                $(".back_right").delay(500).animate({width : '52%'}, 500, function(){
+                    $(".back_right p").fadeIn(500, function(){
+                        $(".back_right h1").delay(200).fadeIn(200, function(){
+                            $(".name_part3").addClass("text-flicker-in-glow")
+                            $(".about").delay(1300).fadeIn("slow")
+                            $(".templet").delay(1400).fadeIn("slow", function(){
+                                $(".name_part3").css("opacity", "1")
+                                $(".name_part3").removeClass("text-flicker-in-glow")
+                            })
+                        })
+                    })
+                })
+            })
+        });
+    })
+})
+
+$(document).ready(function(){
+    $(".start_button").click(function(){
+        $(this).animate({
+            right : "250px",
+        }, 200, function(){
+            $(this).animate({
+                right : "-1000px",
+            }, 500, function(){
+
+            })
+        })
+        $(".name_part1").animate({top : "-1000"}, 800, function(){
+            $(".name_part1").remove()
+        })
+        $(".name_part2").animate({top : "-1000"}, 800, function(){
+            $(".name_part2").remove()
+        })
+        $(".back_right").animate({width : "37%"}, 800, function(){
+            $(".name_part3").addClass("flicker-out-1")
+            $(".about").delay(1000).fadeOut("slow")
+            $(".templet").delay(900).fadeOut("slow", function(){
+                $(".bg_grey").remove()
                 window.scrollTo({top:0,behavior:'smooth'});
-                $("#CV").removeClass("scale-down-center")
-                $("#table_add_infor").delay(700).fadeIn()
                 $("p").empty(".ct");
                 $(".CV_exper_informations li").remove()
                 $(".ad_mail").text("Email")
                 $(".ad_phone").text("Phone number")
                 $(".ad_dayOfBirth").text("Day of birth")
                 $(".ad_address").text("Address")
-                $("#menu").fadeIn()
-                $(".div_around").fadeIn()
-            })    
+                $(".backgr_intro").animate({right : "-5000px"}, 1300, function(){
+                    $("#table_add_infor").delay(700).fadeIn(100)
+                    $(".back_right").fadeOut(1000)
+                    $("#intro_web").fadeOut(1000, function(){
+                        $("#menu").fadeIn()
+                        $(".div_around").fadeIn()
+                    })
+                })
+            })
         })
     })
 })
@@ -200,8 +308,8 @@ function append1(){
     let tx6 = $("<h1></h1>").text($("#add_infor_input_title_exper").val())
     let tx7 = $("<p></p>").text("(" + month + "/" + year + " to " + month2 + "/" + year2 + ")")
     if($("#add_infor_input_title_exper").val() != ""){
-        $("#CV_exper .CV_exper_informations").append(tx1)
-        $("#CV_exper .CV_exper_informations li:last-child").append(tx2, tx7)
+        $("#CV_experience").append(tx1)
+        $("#CV_experience li:last-child").append(tx2, tx7)
         $(".Experience").after(tx4)
         $(".Experience+div").append(tx6, tx5)
         $(".form_add_element_exper").children().not("div.tag_add").not(".button_submit_add").css("display", "none")
@@ -209,7 +317,7 @@ function append1(){
         $("#add_infor_input_exper_date_st").val("")
         $("#add_infor_input_exper_date_en").val("")
         if($("#add_infor_input_exper").val() != ""){
-            $("#CV_exper .CV_exper_informations li:last-child").append(tx3)
+            $("#CV_experience li:last-child").append(tx3)
             $("#add_infor_input_exper").val("")
         }
     }
@@ -375,57 +483,78 @@ function append6(){
 
 $(document).ready(function(){
     $(".review_cv").click(function(){
-        $("#menu").fadeOut()
-        $(".div_around").fadeOut()
-        $(".button_menu_bar").css({
-            height : "0",
-            top: "668px"
-        })
-        $("#CV").removeClass("scale-up-center")
-        $("#table_add_infor").animate({left : "-2000px"}, 300)
-        $("#CV").delay(400).animate({left: "0"}, 500, function(){
-            $("#CV").addClass("scale-down-center")
-            window.scrollTo({top:235,behavior:'smooth'});
-            $(".button_turnback").css("display", "flex")
-            $(".button_turnback_real").css("display", "block")
-        })
+        if($("#CVp2").css("display") == 'block'){
+            $("#footer").fadeOut()
+            $("#head").fadeOut()
+            $("#button_menu").fadeOut()
+            $("#button_turnback").fadeIn(1500)
+            $(".div_around").fadeOut()
+            $(".button_menu_bar").css({
+                height : "0",
+                top: "668px"
+            })
+            $("#CV").removeClass("scale-up-center")
+            $("#CVp2").removeClass("scale-up-center")
+            $("#table_add_infor").animate({left : "-2000px"}, 300)
+            $("#CV").delay(400).animate({left: "-300px"}, 500, function(){
+                $("#CVp2").animate({left: "300px", top : "-=1170px"})
+                $("#CV").addClass("scale-down-center1")
+                $("#CVp2").addClass("scale-down-center1")
+                window.scrollTo({top:235,behavior:'smooth'});   
+            })
+        }else{
+            $("#footer").fadeOut()
+            $("#head").fadeOut()
+            $("#button_menu").fadeOut()
+            $("#button_turnback").fadeIn(1500)
+            $(".div_around").fadeOut()
+            $(".button_menu_bar").css({
+                height : "0",
+                top: "668px"
+            })
+            $("#CV").removeClass("scale-up-center")
+            $("#table_add_infor").animate({left : "-2000px"}, 300)
+            $("#CV").delay(400).animate({left: "0"}, 500, function(){
+                $("#CV").addClass("scale-down-center1")
+                window.scrollTo({top:235,behavior:'smooth'});   
+            })
+        }
     })
 })
 
 $(document).ready(function(){
-    $(".button_turnback_real").mouseenter(function(){
-        $(".button_turnback").animate({left : "0"})
-    })
-    $(".button_turnback").mouseenter(function(){
-        $(".button_turnback").css("left", "0")
-    })
-    $(".button_turnback").mouseleave(function(){
-        $(".button_turnback").animate({left : "-25%"})
+    $("#button_turnback").click(function(){
+        if($("#CVp2").css("display") == 'block'){
+            $("#footer").fadeIn()
+            $("#head").fadeIn()
+            $("#button_menu").fadeIn()
+            $("#button_turnback").fadeOut()
+            $(".div_around").fadeIn()
+            $("#CV").addClass("scale-up-center")
+            $("#CV").removeClass("scale-down-center1")
+            $("#CVp2").addClass("scale-up-center")
+            $("#CVp2").removeClass("scale-down-center1")
+            $("#CVp2").animate({top : "+=1170px" ,left: "250px"}, 90)
+            $("#CV").delay(400).animate({left: "250px"}, 500, function(){
+                window.scrollTo({top:0,behavior:'smooth'});
+                $("#table_add_infor").animate({left : "0"}, 500)
+            })
+        }else{
+            $("#footer").fadeIn()
+            $("#head").fadeIn()
+            $("#button_menu").fadeIn()
+            $("#button_turnback").fadeOut()
+            $(".div_around").fadeIn()
+            $("#CV").addClass("scale-up-center")
+            $("#CV").removeClass("scale-down-center1")
+            $("#CV").delay(400).animate({left: "250px"}, 500, function(){
+                window.scrollTo({top:0,behavior:'smooth'});
+                $("#table_add_infor").animate({left : "0"}, 500)
+            })
+        }
     })
 })
 
-$(document).ready(function(){
-    $(".button_turnback").click(function(){
-        $("#menu").fadeIn()
-        $(".div_around").fadeIn()
-        $(".button_turnback").css("display", "none")
-        $(".button_turnback_real").css("display", "none")
-        $("#CV").addClass("scale-up-center")
-        $("#CV").removeClass("scale-down-center")
-        $("#CV").delay(400).animate({left: "250px"}, 500, function(){
-            window.scrollTo({top:0,behavior:'smooth'});
-            $("#table_add_infor").animate({left : "0"}, 500)
-        })
-    })
-})
-
-
-$(document).on("click", ".dataid",function(){
-    var a = $(this).attr("data")
-    var b = document.getElementById(a)
-    b.remove()
-    $(this).parent().remove()
-});
 
 
 $(document).on("click", ".print_cv", function(){
@@ -473,10 +602,40 @@ $(document).ready(function(){
     })
 })
 
-$(window).on('load',function(){
-    if ($('#loader').length) {
-        $('#loader').delay(300).fadeOut('slow', function () {
-            $(this).remove();
-        });
-    }
+var luu, luu2, save;
+
+$(document).ready(function(){
+    console.log($(".add_infor_button"))
+    $(".add_infor_button").click(function(){
+        luu = $(".height").height()
+        if(luu > 960){
+            $("#CVp2").css("display", "block")
+            save = $(".height .infor_user:last-child")
+            $(".height .infor_user:last-child").remove()
+            $("#CVp2 .main_informationp2").prepend(save)
+            window.scrollTo({top:1200,behavior:'smooth'});
+        }
+    })
 })
+
+
+$(document).on("click", ".dataid",function(){
+    var a = $(this).attr("data")
+    var b = document.getElementById(a)
+    b.remove()
+    $(this).parent().remove()
+    luu2 = $(".main_informationp2 .infor_user:first-child").outerHeight()
+    luu = $(".height").height()
+    console.log(luu2)
+    if((luu + luu2) < 960){
+        save = $(".main_informationp2 .infor_user:first-child")
+        $("#CVp2 .main_informationp2 .infor_user:first-child").remove()
+        $(".height").append(save)
+        if($(".main_informationp2").children()){
+            $("#CVp2").css("display", "none")
+        }else{
+            window.scrollTo({top:700,behavior:'smooth'});
+        }
+        
+    }
+});
